8ab5f2d7de37e81199461e632a02780115e17d9a
malakhamdi2412 <malakhamdi2412@gmail.com>
2026-07-17 19:59:18 +0300

    Update hello.c

--- Task 1/hello.c
+++ Task 1/hello.c
@@ -4,30 +4,28 @@
 int main(void)
 {
     int height;
-    
-    // لوب عشان نضمن إن المستخدم يدخل رقم بين 1 و 8
     do
     {
         height = get_int("Height: ");
     }
     while (height < 1 || height > 8);
 
-    // الـ Loop الرئيسي لطباعة السطور
+    
     for (int i = 0; i < height; i++)
     {
-        // لوب لطباعة المسافات عشان الهرم يروح يمين
+        
         for (int j = 0; j < height - i - 1; j++)
         {
             printf(" ");
         }
         
-        // لوب لطباعة علامات الشباك (#)
+        
         for (int k = 0; k <= i; k++)
         {
             printf("#");
         }
         
-        // ننزل سطر جديد بعد ما السطر يخلص
+       
         printf("\n");
     }
 }
