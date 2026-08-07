da9f5af6dcabd581186c2e2296df52d759005d02
malakhamdi2412 <malakhamdi2412@gmail.com>
2026-08-07 23:19:22 +0300

    Implement JPEG recovery program in recover.c
    This program recovers JPEGs from a memory card image file by reading blocks of data and identifying JPEG signatures.

---
+++ Task4/recover.c
@@ -0,0 +1,63 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+const int BLOCK_SIZE = 512;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    
+    FILE *card = fopen(argv[1], "r");
+    if (card == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+  
+    uint8_t buffer[BLOCK_SIZE];
+    FILE *img = NULL;
+    char filename[8];
+    int image_count = 0;
+
+    
+    while (fread(buffer, 1, BLOCK_SIZE, card) == BLOCK_SIZE)
+    {
+      
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+          
+            if (img != NULL)
+            {
+                fclose(img);
+            }
+
+            
+            sprintf(filename, "%03i.jpg", image_count);
+
+          
+            img = fopen(filename, "w");
+            image_count++;
+        }
+
+        
+        if (img != NULL)
+        {
+            fwrite(buffer, 1, BLOCK_SIZE, img);
+        }
+    }
+
+    
+    if (img != NULL)
+    {
+        fclose(img);
+    }
+    fclose(card);
+
+    return 0;
+}
