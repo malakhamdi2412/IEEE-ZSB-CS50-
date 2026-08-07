4266ef44ebf9ef097f1ee541e3cb3ded5b82904c
malakhamdi2412 <malakhamdi2412@gmail.com>
2026-08-07 23:15:24 +0300

    Add image processing functions in helpers.c
    Implement functions for image processing: grayscale, sepia, reflect, and blur.

---
+++ Task4/helpers.c
@@ -0,0 +1,93 @@
+#include "helpers.h"
+#include <math.h>
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
+}
+
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+
+            int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+            int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+
+            image[i][j].rgbtRed = (sepiaRed > 255) ? 255 : sepiaRed;
+            image[i][j].rgbtGreen = (sepiaGreen > 255) ? 255 : sepiaGreen;
+            image[i][j].rgbtBlue = (sepiaBlue > 255) ? 255 : sepiaBlue;
+        }
+    }
+}
+
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+          
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
+}
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float totalRed = 0, totalGreen = 0, totalBlue = 0;
+            int counter = 0;
+
+          
+            for (int r = -1; r <= 1; r++)
+            {
+                for (int c = -1; c <= 1; c++)
+                {
+                    int currentI = i + r;
+                    int currentJ = j + c;
+                    if (currentI >= 0 && currentI < height && currentJ >= 0 && currentJ < width)
+                    {
+                        totalRed += copy[currentI][currentJ].rgbtRed;
+                        totalGreen += copy[currentI][currentJ].rgbtGreen;
+                        totalBlue += copy[currentI][currentJ].rgbtBlue;
+                        counter++;
+                    }
+                }
+            }
+
+      
+            image[i][j].rgbtRed = round(totalRed / counter);
+            image[i][j].rgbtGreen = round(totalGreen / counter);
+            image[i][j].rgbtBlue = round(totalBlue / counter);
+        }
+    }
+}
